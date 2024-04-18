3f65dbe6ae236179b8190cf28d166cb37c6a9507
IbrahimIbrahim2001 <ii8538098@gmail.com>
2024-04-18 14:05:16 -0400

    first  attempt to fix the 404page on refresh

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,19 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
+import { HashRouter } from "react-router-dom";
 
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <HashRouter>
+      <QueryClientProvider client={client}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </HashRouter>
   </React.StrictMode>
 );
